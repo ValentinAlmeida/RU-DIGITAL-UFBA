04fef1ecf7936531c43fd64b50d25a06b522228c
Valentin Almeida <valentinfalmeida@hotmail.com>
2023-12-12 20:19:05 -0300

    iniciando com calendarios

---
+++ API/API/CalendarioView.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarioView.swift
+//  API
+//
+//  Created by Student14_02 on 12/12/23.
+//
+
+import SwiftUI
+
+struct CalendarioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalendarioView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarioView()
+    }
+}
