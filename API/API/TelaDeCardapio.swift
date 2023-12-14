0f0f1fac971b5954e577980d13c46fd81f300211
salo789dev <edvanesalomao03@gmail.com>
2023-12-14 19:29:19 -0300

    Tela de carpio scrollview

--- API/API/TelaDeCardapio.swift
+++ API/API/TelaDeCardapio.swift
@@ -16,87 +16,99 @@ struct TelaDeCardapio: View {
             
             Color("Amarelo")
                 .ignoresSafeArea()
-            VStack{
-                VStack {
-                    
-                    
-                    Text("Cardápio do Almoço")
-                        .font(.title)
-                        .padding()
-                        .foregroundColor(.white)
-                    if let cardapioManha = pedidos.first(where: { $0.turno == 0 }), isToday(cardapioManha.data) {
-                        // Exibir detalhes do cardápio da manhã
-                        Text("Turno: Manhã")
-                        Text("Bebida: \(cardapioManha.bebida ?? "")")
-                        
-                        Text("Acompanhamento: \(cardapioManha.acompanhamento?.joined(separator: ", ") ?? "")")
-                            .padding(.horizontal, 12)
-                        Text("Salada: \(cardapioManha.salada?.joined(separator: ", ") ?? "")")
-                        Text("Proteína: \(cardapioManha.proteina ?? "")")
-                        Text("Vegetariana: \(cardapioManha.vegetariana ?? "")")
-                        Text("Sobremesa: \(cardapioManha.sobremesa ?? "")")
-                        
-                        // Adicionar botões de editar e deletar
-                        if tipoUsuario == .admin {
-                            HStack {
-                                Button("Deletar") {
-                                    // Ação de deletar
-                                    // Implemente a lógica desejada para a exclusão
-                                }
-                                .padding()
-                                .foregroundColor(.white)
-                            }
-                        }
-                    } else {
-                        Text("Cardápio da Manhã não disponível para hoje.")
-                    }
-                }.background(Color("LaranjaFraco"))
-                .cornerRadius(20)
-                .shadow(radius: 10)
-                .padding()
-                //arredondamento
-                //shadow
+            ScrollView{
                 VStack{
-                    
-                    Text("Cardápio do Jantar")
-                        .font(.title)
-                        .padding()
-                        .foregroundColor(.white)
-                    
-                    //Adicionar scrollview
-                    
-                    if let cardapioTarde = pedidos.first(where: { $0.turno == 1 }), isToday(cardapioTarde.data) {
-                        // Exibir detalhes do cardápio da tarde
-                        Text("Turno: Tarde")
-                        Text("Bebida: \(cardapioTarde.bebida ?? "")")
-                        Text("Acompanhamento: \(cardapioTarde.acompanhamento?.joined(separator: ", ") ?? "")")
-                            .padding(.horizontal, 12)
-                        Text("Salada: \(cardapioTarde.salada?.joined(separator: ", ") ?? "")")
-                        Text("Proteína: \(cardapioTarde.proteina ?? "")")
-                        Text("Vegetariana: \(cardapioTarde.vegetariana ?? "")")
-                        Text("Sobremesa: \(cardapioTarde.sobremesa ?? "")")
+                    VStack {
                         
-                        // Adicionar botões de editar e deletar
-                        HStack {
-                            Button("Deletar") {
-                                // Ação de deletar
-                                // Implemente a lógica desejada para a exclusão
-                            }
+                        
+                        Text("Cardápio do Almoço")
+                            .font(.title)
                             .padding()
                             .foregroundColor(.white)
+                        if let cardapioManha = pedidos.first(where: { $0.turno == 0 }), isToday(cardapioManha.data) {
+                            // Exibir detalhes do cardápio da manhã
+                            Text("Turno: Manhã")
+                            Text("Bebida: \(cardapioManha.bebida ?? "")")
+                            
+                            Text("Acompanhamento: \(cardapioManha.acompanhamento?.joined(separator: ", \n") ?? "")")
+                                .lineLimit(2...5)
+                                .multilineTextAlignment(.center)
+                                .padding(.horizontal, 12)
+                            Text("Salada: \(cardapioManha.salada?.joined(separator: ", ") ?? "")")
+                            Text("Proteína: \(cardapioManha.proteina ?? "")")
+                            Text("Vegetariana: \(cardapioManha.vegetariana ?? "")")
+                            Text("Sobremesa: \(cardapioManha.sobremesa ?? "")")
+                            
+                            // Adicionar botões de editar e deletar
+                            if tipoUsuario == .admin {
+                                HStack {
+                                    Button("Deletar") {
+                                        // Ação de deletar
+                                        // Implemente a lógica desejada para a exclusão
+                                    }
+                                    .padding()
+                                    .foregroundColor(.white)
+                                }
+                            }
+                        } else {
+                            Text("Cardápio da Manhã não disponível para hoje.")
                         }
-                    } else {
-                        Text("Cardápio da Tarde não disponível para hoje.")
+                        Spacer()
+                    }.background(Color("LaranjaFraco"))
+                        .cornerRadius(20)
+                        .shadow(radius: 10)
+                        .padding()
+                    //arredondamento
+                    //shadow
+                    VStack{
+                        
+                        Text("Cardápio do Jantar")
+                            .font(.title)
+                            .padding()
+                            .foregroundColor(.white)
+                        
+                        //Adicionar scrollview
+                        
+                        if let cardapioTarde = pedidos.first(where: { $0.turno == 1 }), isToday(cardapioTarde.data) {
+                            // Exibir detalhes do cardápio da tarde
+                            Text("Turno: Tarde")
+                            Text("Bebida: \(cardapioTarde.bebida ?? "")")
+                            Text("Acompanhamento: \(cardapioTarde.acompanhamento?.joined(separator: ", \n") ?? "")")
+                                .lineLimit(2...5)
+                                .multilineTextAlignment(.center)
+                                .padding(.horizontal, 12)
+                            
+                            
+                            Text("Salada: \(cardapioTarde.salada?.joined(separator: ", ") ?? "")")
+                            Text("Proteína: \(cardapioTarde.proteina ?? "")")
+                            Text("Vegetariana: \(cardapioTarde.vegetariana ?? "")")
+                            Text("Sobremesa: \(cardapioTarde.sobremesa ?? "")")
+                            
+                            // Adicionar botões de editar e deletar
+                            if tipoUsuario == .admin {
+                                HStack {
+                                    Button("Deletar") {
+                                        // Ação de deletar
+                                        // Implemente a lógica desejada para a exclusão
+                                    }
+                                    .padding()
+                                    .foregroundColor(.white)
+                                }
+                            }
+                        } else {
+                            Text("Cardápio da Tarde não disponível para hoje.")
+                        }
+                        Spacer()
                     }
-                }
-                .background(Color("LaranjaForte"))
-                .cornerRadius(20)
-                .shadow(radius: 10)
-                //arredondamento
-                //shadow
-                .padding()
+                    .background(Color("LaranjaForte"))
+                    .cornerRadius(20)
+                    .shadow(radius: 10)
+                    //arredondamento
+                    //shadow
+                    .padding()
+                } //.padding(.top, 20)
+                
             }
-            
         }
     }
     private func isToday(_ date: Date?) -> Bool {
